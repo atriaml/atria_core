c3e911439354bdbb1ec484180007f7ece52c0f15
saifullah3396 <saifullah3396@gmail.com>
2026-08-03 09:30:29 +0200

    cleanup

--- test2.py
+++
@@ -1,9 +0,0 @@
-from atria_core.types._data_instance._image_instance import ImageInstance
-from atria_core.types._generic._annotations import (
-    AnnotationType,
-    ClassificationAnnotation,
-)
-
-image = ImageInstance(sample_id="s1", image=None)
-image.add_annotation(ClassificationAnnotation(label_value=0, label_name="label1"))
-ann = image.get_annotation_by_type(AnnotationType.classification)
